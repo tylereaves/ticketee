b096991e840bfac9e6df72f544faaaf6f89dbb50
Tyler Eaves <teaves@reflector.com>
2011-12-19 14:28:20 -0500

    User add/edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
   def index
     @users = User.all(:order =>"email")
   end
@@ -7,6 +8,7 @@ class Admin::UsersController < Admin::BaseController
   end
   def create
     @user = User.new(params[:user])
+    set_admin
     if @user.save
       flash[:notice] = "User has been created."
       redirect_to admin_users_path
@@ -15,4 +17,36 @@ class Admin::UsersController < Admin::BaseController
       render :action => "new"
     end
   end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    set_admin
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been updated."
+      render :action => "edit"
+    end
+  end
+  private
+  def find_user
+    @user = User.find(params[:id])
+  end
+  def set_admin
+    @user.admin = params[:user][:admin] == "1"
+  end
+
+
 end
