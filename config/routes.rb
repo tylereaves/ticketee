1ac677f73141e9c695faf16d043d69954fe6f27f
Tyler Eaves <teaves@reflector.com>
2011-12-19 10:10:44 -0500

    Admin create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Ticketee::Application.routes.draw do
-
   devise_for :users
 
   root :to => 'projects#index'
   resources :projects do
     resources :tickets
+  end
+  namespace :admin do
+    root :to =>'base#index'
+    resources :users
   end
 end
---
+++ spec/views/admin/users/index.html.haml_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "users/index.html.haml" do
+
+end
